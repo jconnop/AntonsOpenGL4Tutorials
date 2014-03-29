3606ff6bb7aa49aaddea1e04d04fd6e65aa14faa
Jesse Connop <jconnop@gmail.com>
2014-03-30 04:55:57 +1300

    Changed GL profile to forward compat

--- Open.gl_Tutorial/Renderer.cpp
+++ Open.gl_Tutorial/Renderer.cpp
@@ -46,6 +46,7 @@ namespace Fal
 		// Set up window options
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glMajor);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glMinor);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
